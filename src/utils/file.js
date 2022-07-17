3d379c3dc2681dc5d3f7c6beb214f32b00d7ee59
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-17 08:23:22 +0300

    Add ensureDirectoryExistence function

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
-const path = require('path');
+const fsPath = require('path');
 const { DEFAULT_CONTENT } = require('./constants');
 
 // create file if not exists using arrow function
 const createDatabase = (path) => {
     if (!fs.existsSync(path)) {
+        ensureDirectoryExistence(path);
         fs.writeFileSync(path, JSON.stringify(DEFAULT_CONTENT, null, 2));
     }
+}
+
+const ensureDirectoryExistence = (path) => {
+    const dirname = fsPath.dirname(path);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
 }
 
 const updateDatabase = (path, data) => {
