fb2e4563cd64779d78e482bf4a7c75c68fe32c38
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-17 08:20:49 +0300

    Pretty :rainbow:

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,21 @@
 declare class JsonDB<T = any> {
-    constructor(path: string, options?: {
-        uuid?: boolean,
-        primaryKey?: string,
-        minify?: boolean,
-        timestamp?: boolean,
-        stringify?: (o:T) => string,
-        parse?: (s:string) => (T | undefined),
-    });
+  constructor(
+    path: string,
+    options?: {
+      uuid?: boolean;
+      primaryKey?: string;
+      minify?: boolean;
+      timestamp?: boolean;
+      stringify?: (o: T) => string;
+      parse?: (s: string) => T | undefined;
+    }
+  );
 
-    add(key: string, value: T): boolean;
-    findById(key: string, id: number): T | undefined;
-    findAll(key: string): T[];
-    update(key: string, id: number, value: T): boolean;
-    delete(key: string, id: number): boolean;
-    deleteAll(key: string): boolean;
-    clear(): boolean;
+  add(key: string, value: T): boolean;
+  findById(key: string, id: number): T | undefined;
+  findAll(key: string): T[];
+  update(key: string, id: number, value: T): boolean;
+  delete(key: string, id: number): boolean;
+  deleteAll(key: string): boolean;
+  clear(): boolean;
 }
