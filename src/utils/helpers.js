68d0208edd117ea08b1b1c3fcf1d413a3bbb2522
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-17 08:20:35 +0300

    Remove unused functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19,9 +19,7 @@ const validatePath = (path) => {
     }
 }
 
-const createPath = (path) => {
-    return fsPath.join(process.cwd(), validatePath(path))
-}
+const createPath = (path) => fsPath.join(process.cwd(), validatePath(path))
 
 const getDB = (path) => {
     let db = {};
@@ -38,44 +36,6 @@ const getDB = (path) => {
     return db;
 }
 
-const writeDB = (path, db) => {
-    fs.writeFileSync(path, JSON.stringify({ data: db }));
-}
-
-const fileExistence = (path) => {
-    try {
-        return fs.statSync(path);
-    } catch (e) {
-        if (e.code === 'ENOENT') {
-            /* File doesn't exist */
-            createFile(path);
-            return;
-        } else if (e.code === 'EACCES') {
-            throw new Error(`Cannot access path "${path}".`);
-        } else {
-            // Other error
-            throw new Error(`Error while checking for existence of path "${path}": ${e}`);
-        }
-    }
-}
-
-const createFile = (path) => {
-    try {
-        fs.writeFileSync(path, JSON.stringify(DEFAULT_OPTIONS));
-    } catch (e) {
-        throw new Error(`Error while creating file "${path}": ${e}`);
-    }
-}
-
-const fileExists = (path) => {
-    try {
-        fs.accessSync(path, fs.constants.R_OK, fs.constants.W_OK);
-        return true;
-    } catch (e) {
-        throw new Error(`Cannot read & write on path "${path}". Check permissions!`);
-    }
-}
-
 const setDefaultOptions = (options) => Object.assign({}, DEFAULT_OPTIONS, options);
 
 const currentDate = () => new Date().toISOString();
@@ -85,11 +45,7 @@ module.exports = {
     validateJSON,
     validatePath,
     setDefaultOptions,
-    fileExistence,
-    createFile,
-    fileExists,
     createPath,
     getDB,
-    writeDB,
     currentDate
 }
