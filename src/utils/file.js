b53603942d82a36350092ee2dfd36b9548168b2b
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-22 08:59:19 +0300

    Pretty :rainbow:

--- src/utils/file.js
+++ src/utils/file.js
@@ -6,35 +6,35 @@ const { DEFAULT_CONTENT } = require('./constants');
  * @param {string} path
  */
 const createDatabase = (path) => {
-    if (!fs.existsSync(path)) {
-        ensureDirectoryExistence(path);
-        fs.writeFileSync(path, JSON.stringify(DEFAULT_CONTENT, null, 2));
-    }
-}
+  if (!fs.existsSync(path)) {
+    ensureDirectoryExistence(path);
+    fs.writeFileSync(path, JSON.stringify(DEFAULT_CONTENT, null, 2));
+  }
+};
 
 /**
  * @param {string} path
  * @returns {boolean}
  */
 const ensureDirectoryExistence = (path) => {
-    const dirname = fsPath.dirname(path);
-    if (fs.existsSync(dirname)) {
-        return true;
-    }
-    ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
-}
+  const dirname = fsPath.dirname(path);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+};
 
 /**
  * @param {string} path
  * @param {Object} data
  */
 const updateDatabase = (path, data) => {
-    data.updated_at = new Date().toISOString();
-    fs.writeFileSync(path, JSON.stringify(data, null, 2));
-}
+  data.updated_at = new Date().toISOString();
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+};
 
 module.exports = {
-    createDatabase,
-    updateDatabase
-}
+  createDatabase,
+  updateDatabase
+};
