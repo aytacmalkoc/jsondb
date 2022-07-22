7db086a6116c72b7da4184f1d3c6df5e56b26c84
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-22 08:55:33 +0300

    Add JSDoc blocks

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,23 +2,23 @@
  * @type {{minify: boolean, uuid: boolean, primaryKey: string, timestamp: boolean}}
  */
 const DEFAULT_OPTIONS = {
-    uuid: true,
-    primaryKey: 'id',
-    minify: false,
-    timestamp: true
-}
+  uuid: true,
+  primaryKey: 'id',
+  minify: false,
+  timestamp: true
+};
 
 /**
  * @type {{updated_at: string, data: {}, created_at: string, version: number}}
  */
 const DEFAULT_CONTENT = {
-    version: 1,
-    created_at: new Date().toISOString(),
-    updated_at: new Date().toISOString(),
-    data: {}
-}
+  version: 1,
+  created_at: new Date().toISOString(),
+  updated_at: new Date().toISOString(),
+  data: {}
+};
 
 module.exports = {
-    DEFAULT_OPTIONS,
-    DEFAULT_CONTENT
-}
+  DEFAULT_OPTIONS,
+  DEFAULT_CONTENT
+};
